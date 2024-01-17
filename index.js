95336610b9c642df743f110934a4235afca11997
Abhishek Kashyap <100584650+Actuallyabhi87@users.noreply.github.com>
2024-01-17 14:02:30 +0530

    gofood app
    deploy on vercel

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const app = express();
 const path = require("path");
 
 const port = 5000;
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 app.use(cors());
-
 app.use(express.json());
 app.use(bodyparser.json());
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
-app.use(express.static(path.join(__dirname, "./foodapp/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./foodapp/build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
+// app.get("/", (req, res) => {
+//   app.use(express.static(path.resolve(__dirname, "client", "build")));
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 mongodb();
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server Started`));
